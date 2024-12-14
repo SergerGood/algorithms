a53d42baaffefa83bfb19a414fc8d3708ae6bef9
sergergood <serzher@inbox.ru>
2024-12-14 10:49:33 +0300

    Связные списки. Решение

--- 04_Standard_Structures/List/src/com/skillbox/List.java
+++ 04_Standard_Structures/List/src/com/skillbox/List.java
@@ -1,33 +1,65 @@
 package com.skillbox;
 
+import java.text.MessageFormat;
+
 public class List {
     public class Node {
         int x;
         Node next;
+
         public Node(int x) {
             this.x = x;
             this.next = null;
         }
+
+        @Override
+        public String toString() {
+            return MessageFormat.format("x = {0}, next x = {1}", x, next != null ? next.x : "null");
+        }
     }
 
     // Pointer to the beginning of the list
-    Node begin;
+    Node firstNode;
+    Node lastNode;
+    int length;
+
+    public void push(int x) {
+        var newNode = new Node(x);
+
+        if (firstNode == null) {
+            firstNode = newNode;
+        } else {
+            lastNode.next = newNode;
+        }
+
+        lastNode = newNode;
+        length++;
+    }
 
     // Adds element to the beginning of the list
     public void push_front(int x) {
-        /* TODO IMPLEMENT THIS */
+        var newNode = new Node(x);
+        newNode.next = firstNode;
+
+        firstNode = newNode;
+
+        if (lastNode == null) {
+            lastNode = firstNode;
+        }
+
+        length++;
     }
 
     public List() {
-        begin = null;
+        firstNode = null;
     }
 
     // This function could be useful for debugging and testing.
     public void print() {
-        Node n = this.begin;
-        while (n != null) {
-            System.out.print(n.x + " ");
-            n = n.next;
+        Node node = this.firstNode;
+        while (node != null) {
+            System.out.print(node.x + " ");
+            node = node.next;
         }
         System.out.println();
     }
@@ -35,20 +67,39 @@ public class List {
     // This function should return copy of the list where every second element is removed. Initial list should not be changed.
     // E.g. if we run copyEverySecond on list [1, 2, 3, 4, 5, 6, 7, 100, 120, 162, 0, 1] new list with values [1, 3, 5, 7, 120, 0] should be returned.
     public List copyEverySecond() {
-        /* TODO IMPLEMENT THIS */
-        return new List();
+        List result = new List();
+        result.push(firstNode.x);
+
+        var step = 1;
+        var currentNode = firstNode.next;
+
+        while (currentNode != null) {
+            if (step % 2 == 0 ) {
+                result.push(currentNode.x);
+            }
+            currentNode = currentNode.next;
+            step++;
+        }
+
+        return result;
     }
 
     // Returns number of elements in list
     public int getSize() {
-        /* TODO IMPLEMENT THIS */
-        return 0;
+        return length;
     }
 
     // This function converts our list to an array. New array is created with values the same as in list.
     public int[] toArray() {
-        /* TODO IMPLEMENT THIS */
-        return new int[0];
+        var array = new int[length];
+        var currentNode = firstNode;
+
+        for (int i = 0; i < length; i++) {
+            array[i] = currentNode.x;
+            currentNode = currentNode.next;
+        }
+
+        return array;
     }
 
     // This function removes elements x.next from the list
@@ -60,29 +111,62 @@ public class List {
     // This function inserts new element with value val right after the element x.
     // O(1) time is expected
     public void insertAfter(Node x, int val) {
-        /* TODO IMPLEMENT THIS */
+        var newNode = new Node(val);
+
+        newNode.next = x.next;
+        x.next = newNode;
+
+        length++;
     }
 
     // This function removes all elements from the list that are divisible by x.
     // E.g. list {1, 2, 3, 4, 4, 10, 7}  after filterDivisible(2) would look like {1, 3, 7}.
     // O(N) time is expected.
     public void filterDivisible(int x) {
-        /* TODO IMPLEMENT THIS */
+        while (firstNode != null && firstNode.x % x == 0) {
+            firstNode = firstNode.next;
+            length--;
+        }
+
+        if (firstNode == null) {
+            return;
+        }
+
+        var currentNode = firstNode;
+        while (currentNode.next != null) {
+
+            if (currentNode.next.x % x == 0) {
+                currentNode.next = currentNode.next.next;
+                length--;
+            }
+            else {
+                currentNode = currentNode.next;
+            }
+        }
     }
 
     // This function returns Node from the list by index. O(N) time is expected.
     public Node getAt(int index) {
-        /* TODO IMPLEMENT THIS */
-        return null;
+        var currentNode = firstNode;
+        var currentIndex = 0;
+
+        while (currentNode != null) {
+            if (currentIndex == index) {
+                break;
+            }
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+
+        return currentNode;
     }
 
     // This function creates List from an array
     public static List fromArray(int[] a) {
-        List l = new List();
+        List list = new List();
         for (int i = a.length - 1; i >= 0; --i) {
-            l.push_front(a[i]);
+            list.push_front(a[i]);
         }
-        return l;
+        return list;
     }
-
 }
--- 04_Standard_Structures/Stack/src/com/skillbox/Stack.java
+++ 04_Standard_Structures/Stack/src/com/skillbox/Stack.java
@@ -3,7 +3,7 @@ package com.skillbox;
 // Stack implemented using dynamic array inside
 public class Stack {
     // Array where we store the data
-    private long[] a;
+    private long[] array;
     // Number of elements that we actually store in the array. <= a.length
     private int size;
 
@@ -14,29 +14,47 @@ public class Stack {
     // This function is called when it's not enough memory to fit new elements.
     // It creates new long array and copies all the elements there.
     public void reallocate() {
-        /* IMPLEMENT THIS */
+        var newArray = new long[size * 2];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+
+        array = newArray;
     }
 
     public Stack() {
-        a = new long[10];
+        array = new long[10];
         size = 0;
     }
 
     // Adds element to the end of the stack
     public void push_back(long x) {
-        /* IMPLEMENT THIS */
+        if (size == array.length) {
+            reallocate();
+        }
+
+        array[size] = x;
+        size++;
     }
 
     // Removes last element from the stack and returns its value
     public long pop_back() {
-        /* IMPLEMENT THIS */
-        return 0;
+        if (size == 0) {
+            return 0;
+        }
+
+        var currentPosition = size - 1;
+        var removedValue = array[currentPosition];
+
+        array[currentPosition] = 0;
+        size--;
+
+        return removedValue;
     }
 
     // Returns value of the last element in the stack
     public long top() {
-        /* IMPLEMENT THIS */
-        return 0;
+        return array[size];
     }
 
     // Calculates the result of reversed polish notation. https://en.wikipedia.org/wiki/Reverse_Polish_notation
@@ -44,11 +62,47 @@ public class Stack {
     // Only + - * should be supported.
     // Example: calcPolish("1 2 3 * -") should return -5 | because (1 - (2 * 3))
     public static long calcPolish(String s) {
-        /* IMPLEMENT THIS */
-        return 0;
+        var stack = new Stack();
+        var elements = s.split(" ");
+
+        for (int i = 0; i < elements.length; i++) {
+            var element = elements[i];
+
+            if (isOperation(element)) {
+                var secondOperator = stack.pop_back();
+                var firstOperator = stack.pop_back();
+
+                if (isPlus(element)) {
+                    var result = firstOperator + secondOperator;
+                    stack.push_back(result);
+                } else if (isSign(element)) {
+                    var result = firstOperator * secondOperator;
+                    stack.push_back(result);
+                } else if (isMinus(element)) {
+                    var result = firstOperator - secondOperator;
+                    stack.push_back(result);
+                }
+            } else {
+                stack.push_back(Integer.parseInt(element));
+            }
+        }
+
+        return stack.pop_back();
     }
 
+    public static boolean isOperation(String symbol) {
+        return isMinus(symbol) || isPlus(symbol) || isSign(symbol);
+    }
 
+    public static boolean isMinus(String symbol) {
+        return symbol.equals("-");
+    }
 
+    public static boolean isPlus(String symbol) {
+        return symbol.equals("+");
+    }
 
+    public static boolean isSign(String symbol) {
+        return symbol.equals("*");
+    }
 }
